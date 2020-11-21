3fcb0791c5a8541ee7926aedbe0e9edaa5897575
João Pereira <joaopereira.eng.tec@gmail.com>
2020-11-21 11:35:31 +0000

    Adds functions setListeners to kown what buttonCancel and buttonNewContact does

--- src/ui/ContactForm.java
+++ src/ui/ContactForm.java
@@ -1,6 +1,9 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ContactForm extends JFrame {
 
@@ -13,8 +16,33 @@ public class ContactForm extends JFrame {
     public ContactForm() {
 
         setContentPane(rootPanel);
-        setSize(500,250);
+        setSize(500, 250);
         setVisible(true);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); // Size Screen
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Close Window
+
+        setListeners();
+
+    }
+
+    private void setListeners() {
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainForm();
+                dispose();
+            }
+        });
 
     }
 }
--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
     private JPanel rootPanel;
@@ -20,6 +22,25 @@ public class MainForm extends JFrame {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Close Window
 
+        setListeners();
+
+    }
+
+    private void setListeners() {
+        buttonNewContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ContactForm();
+                dispose();
+            }
+        });
+
+        buttonRemove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
 }
